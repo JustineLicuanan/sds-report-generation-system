98da155608239d68ccbd158d4f5604e12a87b364
Justine Licuanan <70696164+JustineLicuanan@users.noreply.github.com>
2023-11-16 10:16:27 +0800

    revert auth middlewares & redirects

--- src/utils/auth-redirects.ts
+++ src/utils/auth-redirects.ts
@@ -1,30 +1,30 @@
-// import { UserRole } from '@prisma/client';
+import { UserRole } from '@prisma/client';
 import { type GetServerSidePropsResult } from 'next';
 import { type Session } from 'next-auth';
 
-// import { paths } from '~/meta';
+import { paths } from '~/meta';
 
 export const authRedirects = {
-  admin: (_authSession) => {
-    // if (!authSession?.user) {
-    //   return { redirect: { destination: paths.SIGN_IN, permanent: false } };
-    // }
+  admin: (authSession) => {
+    if (!authSession?.user) {
+      return { redirect: { destination: paths.SIGN_IN, permanent: false } };
+    }
 
-    // if (authSession?.user.role !== UserRole.ADMIN) {
-    //   return { notFound: true };
-    // }
+    if (authSession?.user.role !== UserRole.ADMIN) {
+      return { notFound: true };
+    }
 
     return { props: {} };
   },
 
-  organization: (_authSession) => {
-    // if (!authSession?.user) {
-    //   return { redirect: { destination: paths.SIGN_IN, permanent: false } };
-    // }
+  organization: (authSession) => {
+    if (!authSession?.user) {
+      return { redirect: { destination: paths.SIGN_IN, permanent: false } };
+    }
 
-    // if (authSession?.user.role !== UserRole.STUDENT_LEADER) {
-    //   return { notFound: true };
-    // }
+    if (authSession?.user.role !== UserRole.STUDENT_LEADER) {
+      return { notFound: true };
+    }
 
     return { props: {} };
   },
