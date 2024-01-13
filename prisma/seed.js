5299ef49d43b4e8dd37132aced83227aaed3bc20
Justine Licuanan <119480395+JustineLicu@users.noreply.github.com>
2024-01-13 12:56:25 +0000

    fix: ReportSignatory not seeding

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,11 +1,13 @@
 const { db } = require('./db');
 const { settingsSeeder } = require('./seeders/settings');
+const { reportSignatorySeeder } = require('./seeders/report-signatory');
 const { userSeeder } = require('./seeders/user');
 
 (async () => {
   try {
     // Seeders
     await settingsSeeder();
+    await reportSignatorySeeder();
     await userSeeder();
 
     // Disconnect after seeding
